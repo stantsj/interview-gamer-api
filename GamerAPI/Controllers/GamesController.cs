bb49c7adc8dd1554a562a2ac30fa4398beaa76bb
stantsj <joseph.stants@gmail.com>
2022-02-09 18:36:41 -0500

    Add GamesController

---
+++ GamerAPI/Controllers/GamesController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GamerAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GamesController : ControllerBase
+    {
+        // GET: api/<GamesController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<GamesController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+    }
+}
