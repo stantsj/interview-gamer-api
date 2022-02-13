9f63f27b990263f4138e9eb3aca5e874feb9cf8c
stantsj <joseph.stants@gmail.com>
2022-02-13 13:12:09 -0500

    Fix GameRequestDTO properties

--- GamerAPI/Models/GameRequestDTO.cs
+++ GamerAPI/Models/GameRequestDTO.cs
@@ -5,11 +5,5 @@ namespace GamerAPI.Models
     public class GameRequestDTO
     {
         public int GameId { get; set; }
-        public string Name { get; set; }
-        public int Added { get; set; }
-        public int? Metacritic { get; set; }
-        public decimal Rating { get; set; }
-        public string Released { get; set; }
-        public string Updated { get; set; }
     }
 }
